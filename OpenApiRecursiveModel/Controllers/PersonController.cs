b6aabf91ecd9911a320df9e8ad26fead87dcbd62
Damir Arh <damir.arh@gmail.com>
2024-08-31 12:24:20 +0200

    Add project files.

---
+++ OpenApiRecursiveModel/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenApiRecursiveModel.Models;
+
+namespace OpenApiRecursiveModel.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PersonController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<Person> Get()
+    {
+        var person = new Person("John", "Doe")
+        {
+            Children =
+            [
+                new Person("Jane", "Doe")
+                {
+                    Children = [new Person("Alice", "Doe"), new Person("Bob", "Doe"),]
+                },
+                new Person("Jack", "Doe"),
+            ]
+        };
+        return Ok(person);
+    }
+}
---
+++ OpenApiRecursiveModel/Models/Person.cs
@@ -0,0 +1,6 @@
+﻿namespace OpenApiRecursiveModel.Models;
+
+public record Person(string FirstName, string LastName)
+{
+    public List<Person> Children { get; set; } = [];
+}
